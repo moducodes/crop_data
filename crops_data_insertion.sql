8bb4282fd3e300847e8a1bad4637e15fb9857e2a
moducodes <patilvj13@gmail.com>
2020-11-01 22:34:06 +0530

    crop database

---
+++ crops_data_insertion.sql
@@ -0,0 +1,141 @@
+INSERT INTO crops_data.crops_crop (
+	name, expected_harvest, expected_expenditure, expected_income,
+    soil_type, irrigation_type, water_requirement, description
+) VALUES (
+	"Cotton",
+    "10-12 quintal/acre",
+    40558,
+    86330,
+    "black cotton soil",
+    "flood - at 10-12 days int",
+    "650-750 mm of rainfall",
+    "Cotton is one of the most important fibre and cash crop of India as well as of entire world. It plays a dominant role in the industrial and agricultural economy of the country. It provides the basic raw material to cotton textile industry. In India it provides direct livelihood to 6 million farmers and about 40-50 million people are employed in cotton trade and its processing. Cotton is a water thirsty crop and around 6% of the water for irrigation in is used for its cultivation. In India, it is grown on a large scale in Maharashtra, Gujarat, Karnataka, Madhya Pradesh, Punjab, Rajasthan, Haryana, Tamil Nadu and Uttar Pradesh. Gujarat is the largest producer of cotton followed by Maharashtra and Punjab. It is vital kharif crop of Punjab. The average lint yield of the State is around 697 kg per hectare."
+);
+
+INSERT INTO crops_data.crops_crop (
+	name, expected_harvest, expected_expenditure, expected_income,
+    soil_type, irrigation_type, water_requirement, description
+) VALUES (
+	"Brinjal",
+    "60 quintal/acre",
+    39166,
+    114000,
+    "sandy loam soil",
+    "drip",
+    "600-1000mm of rainfall",
+    "Brinjal or Eggplant is an important crop of sub tropics and tropics. The name brinjal is popular in Indian subcontinents and is derived from Arabic and Sanskrit whereas the name eggplant has been derived from the shape of the fruit of some varieties, which are white and resemble in shape to chicken eggs. It is also called aubergine (French word) in Europe. The brinjal is of much importance in the warm areas of Far East, being grown extensively in India, Bangladesh, Pakistan, China and the Philippines. It is also popular in Egypt, France, Italy and United States. In India, it is one of the most common, popular and principal vegetable crops grown throughout the country except higher altitudes. It is a versatile crop adapted to different agro-climatic regions and can be grown throughout the year. It is a perennial but grown commercially as an annual crop. A number of cultivars are grown in India, consumer preference being dependent upon fruit colour, size and shape."
+);
+
+INSERT INTO crops_data.crops_crop (
+	name, expected_harvest, expected_expenditure, expected_income,
+    soil_type, irrigation_type, water_requirement, description
+) VALUES (
+	"Cabbage",
+    "100-120 quintal/acre",
+    55450,
+    110000,
+    "sandy loam to clay",
+    "drip - on alternate day",
+    "350-500 mm of rainfall",
+    "The cabbage is a popular cultivar of the species Brassica Family and is used as a leafy green vegetable.The only part of the plant that is normally eaten is the leafy head; more precisely, the spherical cluster of immature leaves, excluding the partially unfolded outer leaves. Cabbage is used in a variety of dishes for its naturally spicy flavour. unfolded outer leaves. Cabbage is used in a variety of dishes for its naturally spicy flavour. The so-called “cabbage head” is widely consumed raw, cooked, or preserved in a great variety of dishes. Cabbage is an excellent source of vitamin C. It also contains significant amounts of glutamine, an amino acid that has anti-inflammatory properties."
+);
+
+INSERT INTO crops_data.crops_crop (
+	name, expected_harvest, expected_expenditure, expected_income,
+    soil_type, irrigation_type, water_requirement, description
+) VALUES (
+	"Maize",
+    "24-80 quintal/acre",
+    26017,
+    44200,
+    "loams and sandy loams",
+    "only at critical stages",
+    "500-800 mm of rainfall",
+    "Maize (Zea mays L) is one of the most versatile emerging crops having wider adaptability under varied agro-climatic conditions. Globally, maize is known as the queen of cereals because it has the highest genetic yield potential among the cereals. It is cultivated on nearly 150 m ha in about 160 countries having wider diversity of soil, climate, biodiversity and management practices that contributes 36 % (782 m t) in the global grain production. The United States of America (USA) is the largest producer of maize contributes nearly 35 % of the total production in the world and maize is the driver of the US economy. The USA has the highest productivity (> 9.6 t ha-1) which is double than the global average (4.92 t ha-1). Whereas, the average productivity in India is 2.43 t ha-1.
+
+In India, maize is the third most important food crops after rice and wheat. According to advance estimate it is cultivated in 8.7 m ha (2010-11) mainly during Kharif season which covers 80% area. Maize in India, contributes nearly 9 % in the national food basket and more than Rs. 100 billion to the agricultural GDP at current prices apart from the generating employment to over 100 million man-days at the farm and downstream agricultural and industrial sectors. In addition to staple food for human being and quality feed for animals, maize serves as a basic raw material as an ingredient to thousands of industrial products that includes starch, oil, protein, alcoholic beverages, food sweeteners, pharmaceutical, cosmetic, film, textile, gum, package and paper industries etc."
+);
+
+INSERT INTO crops_data.crops_crop (
+	name, expected_harvest, expected_expenditure, expected_income,
+    soil_type, irrigation_type, water_requirement, description
+) VALUES (
+	"Paddy",
+    "12-16 quintal per acre",
+    14915,
+    24000,
+    "all types of soil",
+    "flood - needs waterlogging",
+    "more than 800 mm of rainfall",
+    "Rice is the staple food of Goa. The crop is cultivated over an area of 41,970 ha with a production of 118,000 Tonnes annually. The average productivity of the crop is about 2,830 kg/ha.The crop is being cultivated both in Kharif (Sod) and Rabi (Vaigon) seasons, with an area of approximately 67 % and 33%, respectively. The average productivity of Rabi season is relatively higher as compared to Kharif season. The prevailing hot humid tropical climate and soil types varying from sandy loams to clayey types are ideal for rice cultivation. Average land holding size in Goa is relatively less (95% less than 1 ha) and that too fragmented.Further, the socio-economic situations prevailing in the state and non-availability and high cost of labour, lack of mechanization, threshing and processing facilities including marketing infrastructure are threatening the cultivation of this major food crop. The crop is cultivated in three different topographical situations i.e rainfed uplands (morod), rainfed low lands (kher) and coastal saline lands (Khazans)."
+);
+
+INSERT INTO crops_data.crops_crop (
+	name, expected_harvest, expected_expenditure, expected_income,
+    soil_type, irrigation_type, water_requirement, description
+) VALUES (
+	"Fenugreek",
+    "5000 bunches /acre",
+    12065,
+    30000,
+    "loamy or sandy loam & medium black soil",
+    "flood",
+    "350-380 mm of rainfall",
+    "Fenugreek belongs to leguminous family. In local language it is known as Methi, is the most common vegetable grown throughout the country. Leaves are used for vegetable purpose and its seed are used for flavouring purpose. Leaves and seed also have medicinal value, they help to reduce blood sugar and cholesterol. It is also used for fodder purpose. In India, Rajasthan is major fenugreek growing state. Other major fenugreek producing states are, Madhya Pradesh, Tamil Nadu, Rajasthan, Gujarat, Uttar Pradesh and Punjab."
+);
+
+INSERT INTO crops_data.crops_crop (
+	name, expected_harvest, expected_expenditure, expected_income,
+    soil_type, irrigation_type, water_requirement, description
+) VALUES (
+	"Turmeric",
+    "120-150 quintal per acre",
+    77000,
+    275000,
+    "sandy to clay loam soils",
+    "flood - at 8 to 10 days int",
+    "1500 mm of rainfall",
+    "Turmeric is a perennial herb, native of south Asia. It is also known as Indian Saffron is sacred spice of India. It is principal ingredient in Indian culinary and used as flavouring and coloring agent. It is used in drug and cosmetic industry because of its anti-cancer and anti-viral property. Turmeric has special place in religious and ceremonial occasions. Rhizomes are used for propagation. Its leaves are long, broad and of bright green color and flowers are of pale yellow color. India is world largest producer, consumer and exporter of turmeric. In India, Andhra Pradesh, Orissa, West Bengal, Karnataka and Kerala are major producers of Turmeric."
+);
+
+INSERT INTO crops_data.crops_crop (
+	name, expected_harvest, expected_expenditure, expected_income,
+    soil_type, irrigation_type, water_requirement, description
+) VALUES (
+	"Ginger",
+    "60-80 quintal/acre",
+    60000,
+    210000,
+    "well drained sandy to clay loam soils",
+    "drip - alternate day and flood",
+    "1000-1200 mm of rainfall",
+    "Ginger ( Zingiber officinale Rosc.) (Family: Zingiberaceae) is an herbaceous perennial, the rhizomes of which are used as a spice. India is a leading producer of ginger in the world and during 2012 - 13 the country produced 7.45 lakh tonnes of the spice from an area of 157839 hectares. Ginger is cultivated in most of the states in India. However, states namely Karnataka, Orissa, Assam, Meghalaya, Arunachal Pradesh and Gujarat together contribute 65 per cent to the country's total production."    
+);
+
+INSERT INTO crops_data.crops_crop (
+	name, expected_harvest, expected_expenditure, expected_income,
+    soil_type, irrigation_type, water_requirement, description
+) VALUES (
+	"Sugarcane",
+    "400-600 quintal/acre",
+    60000,
+    125000,
+    "sandy-loam, clay-loam, loam or black cotton",
+    "flood - at 6 to 8 days int",
+    "1800-2200 mm of rainfall",
+    "Sugarcane, Saccharum officinarum L. is a perennial gras. It belongs to bamboo family and it is indigenous to India. It is the main source of sugar, jaggery and khandsari. About two-thirds of the total sugarcane produced in India is consumed for making jaggery and khandsari and only one third of it goes to sugar factories. It also provides raw material for manufacturing alcohol. Brazil is largest producer of sugarcane followed by India, China, Thailand, Pakistan and Mexico. In India, Maharashtra is largest producer of sugar and it contributes about 34% of sugar in country followed by Uttar Pradesh."
+);
+
+INSERT INTO crops_data.crops_crop (
+	name, expected_harvest, expected_expenditure, expected_income,
+    soil_type, irrigation_type, water_requirement, description
+) VALUES (
+	"Tomato",
+    "100 quintal/acre",
+    91897,
+    120000,
+    "loamy soil",
+    "drip - on alternate day",
+    "600-1500 mm of rainfall",
+    "Tomato is originated in Peru of South America. It is important commercial vegetable crop of India. It is the second most important crop of world after potato. Fruits are eaten raw or in cooked form. It is rich source of vitamin A, C, potassium and minerals. It is used in soup, juice and ketch up, powder. The major tomato producing states are Bihar, Karnataka, Uttar Pradesh, Orissa, Maharashtra, Andhra Pradesh, Madhya Pradesh and West Bengal. In Punjab state, Amritsar, Ropar, Jalandhar, Hoshiarpur are tomato growing districts."
+);
